a9c5e3aebb6e2890bdc5d0f896db046df68abeca
EdwardM23 <scrith.edward23@gmail.com>
2025-05-29 11:06:33 +0700

    add about us page

---
+++ src/data/companyProfile.js
@@ -0,0 +1,7 @@
+export const companyProfile = {
+    title: "Company Profile",
+    description: [
+        "PT. Karya Artistika Nusaindo is a dynamic, disciplined, and innovative contracting firm specializing in Interior Fit Out, Interior Design, and General Construction Services. Founded in June 2020 by three passionate professionals in Tangerang. Started as a small team that focused solely on Interior Fit Out and limited radius of work range.",
+        "Through dedication, quality commitment, and willingness to grow, PT. Karya Artistika Nusaindo have expanded their services to meet the growing demands of clients. Today, PT. Karya Artistika Nusaindo blends creativity, timeless design, and technical precision to deliver more services and end-to-end construction solutions."
+    ]
+}; 
